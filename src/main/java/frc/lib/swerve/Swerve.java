4cf39dbad8daeed4674957b230d16b7081323441
FryGuy <147878878+pranav1283@users.noreply.github.com>
2024-10-07 18:43:13 -0400

    added a bunch of Swerve methods

--- src/main/java/frc/lib/swerve/Swerve.java
+++ src/main/java/frc/lib/swerve/Swerve.java
@@ -26,6 +26,40 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class Swerve {
-    
+public class Swerve extends SwerveDrivetrain {
+    //private SwerveRequest.ApplyChassisSpeeds autorequest = new SwerveRequest.ApplyChassisSpeeds();
+
+    public Swerve(SwerveDrivetrainConstants drivetrainConstants, SwerveModuleConstants[] moduleConstants){
+        super(drivetrainConstants, moduleConstants);
+    }
+
+    public ChassisSpeeds getChassisSpeeds(){
+        return m_kinematics.toChassisSpeeds(getModuleStates());
+    }
+
+
+    public SwerveModuleState[] getModuleStates(){
+        return getState().ModuleStates != null? getState().ModuleStates : new SwerveModuleState[] {new SwerveModuleState(), new SwerveModuleState(),new SwerveModuleState(),new SwerveModuleState()};
+    }
+
+    public Pose2d getPose2d(){
+        return getState().Pose != null ? getState().Pose : new Pose2d();
+    }
+
+    public SwerveDriveKinematics getDriveKinematics(){
+        return m_kinematics;
+    }
+
+    public void resetPose(Pose2d pose){
+        try{
+            m_stateLock.writeLock().lock();
+            m_odometry.resetPosition(m_pigeon2.getRotation2d(), m_modulePositions, pose);
+        } finally{
+
+        }
+    }
+
+    public Command zeroGyroCommand(){
+        return Commands.runOnce(() -> m_pigeon2.reset());
+    }
 }
