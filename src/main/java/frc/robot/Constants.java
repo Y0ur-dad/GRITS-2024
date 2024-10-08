82272bb43a45e3d80697b6c9a30ca98bcf1743f7
FryGuy <147878878+pranav1283@users.noreply.github.com>
2024-10-07 22:16:09 -0400

    Started the Constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,3 +31,39 @@ import edu.wpi.first.units.Measure;
 import frc.lib.swerve.SwerveModuleConstants;
 import frc.lib.swerve.COTSFalconSwerveConstants;
 import frc.lib.utils.AllianceFlipUtil;
+
+public final class Constants{
+  public static final class SwerveConstants{
+    public static final int pigeonID = 1;
+    public static final boolean invertGyro = true; //Always make sure Gyro is CCW+ CW-
+
+    public static final COTSFalconSwerveConstants chosenModule = COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L2); //Must be tuned to specific robot
+
+    /** Drivetrain Constants 
+     * Must be tuned to specific robot
+    */
+    public static final double trackWidth = Units.inchesToMeters(24);
+    public static final double wheelBase = Units.inchesToMeters(24);
+    public static final double wheelCircumference = chosenModule.wheelCircumference;
+
+    /**
+     * Swerve kinematics
+     * Don't change this unless you're not doing a traditional 4 module rectangle/square swerve
+     */
+    public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+      Mod0.position,
+      Mod1.position,
+      Mod2.position,
+      Mod3.position);
+    
+    /** Module Gear Ratios */
+    public static final double driveGearRatio = chosenModule.driveGearRatio;
+    public static final double angleGearRatio = chosenModule.angleGearRatio;
+
+    /**Motor Inverts*/
+    public static final boolean angleMotorInvert = chosenModule.angleMotorInvert;
+    public static final boolean driveMotorInvert - chosenModule.driveMotorInvert;
+
+
+  }
+}
