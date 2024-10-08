14a77daff36298f5b97720ee0b1884d142f9614a
FryGuy <147878878+pranav1283@users.noreply.github.com>
2024-10-07 21:35:23 -0400

    added pathplanner and the Constants file

---
+++ src/main/java/frc/robot/io/DriverControls.java
@@ -0,0 +1,14 @@
+package frc.robot.io;
+
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.DriverConstants;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.subsystems.Drive;
+
+
+public class DriverControls {
+    
+}
